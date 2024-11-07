21419d1ffe2bdd6dc8c03c91c72b6b80d4e0ac39
mingdue <89075360+rlaalswn222@users.noreply.github.com>
2024-11-08 00:59:46 +0900

    십진법 이진법 바꾸기

---
+++ 241107_py/binary.py
@@ -0,0 +1,18 @@
+num = int(input('이진법으로 바꾸고 싶은 십진법 숫자를 입력하세요: '))
+
+#나머지
+remain = 0
+#이진법 문자열 생성
+binary = ''
+
+# 0인 경우 특별히 처리
+if num == 0:
+    binary = '0'
+
+# num이 0이 될 때까지 루프
+while num > 0:
+    remain = num % 2  # 나머지를 구함
+    binary = str(remain) + binary  # 나머지를 이진법 문자열에 추가
+    num = num // 2  # num에 몫을 저장하여 반복
+
+print(binary)
---
+++ 241107_py/decimal1.py
@@ -0,0 +1,12 @@
+num = int(input('십진수로 바꾸고 싶은 이진수를 입력하세요: '))
+
+#십진수 문자열
+decimal = 0
+
+length = len(str(num))
+num_list = str(num)
+for round in range(length):
+    bin = int(num_list[round]) * (2**(length-round-1))
+    decimal += bin
+
+print(decimal)
